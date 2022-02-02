e744490c6f68b1600310ab55c8a6cf528b9c67e9
Литвиновский Алексей Борисович <LITVINOVSKIY-AB@sibir.FSK-EES.LOCAL>
2022-02-02 16:16:01 +0700

    start

--- src/search-form.ts
+++ src/search-form.ts
@@ -1,6 +1,21 @@
 import { renderBlock } from './lib.js'
 
-export function renderSearchFormBlock () {
+export function renderSearchFormBlock (dateIn:Date, dateOut:Date) {
+ 
+  function frmtDate(date:Date, delimiter:string){
+    const month = date.getMonth() + 1; 
+    const day = date.getDate();    
+    const year  = date.getFullYear();
+    return year + delimiter + ('0' + month).slice(-2) + delimiter + ('0'+ day).slice(-2);
+  }
+
+  const minDateInStr = frmtDate(new Date(Date.now()), '-');  // before
+  dateIn.setDate(dateIn.getDate()+1);
+  
+  const maxDateOutStr = frmtDate(dateOut, '-'); // before
+  dateOut.setDate(dateOut.getDate()+2);
+  
+  
   renderBlock(
     'search-form-block',
     `
@@ -20,11 +35,11 @@ export function renderSearchFormBlock () {
         <div class="row">
           <div>
             <label for="check-in-date">Дата заезда</label>
-            <input id="check-in-date" type="date" value="2021-05-11" min="2021-05-11" max="2021-06-30" name="checkin" />
+            <input id="check-in-date" type="date" value="${frmtDate(dateIn, '-')}" min="${minDateInStr}" max="${maxDateOutStr}" name="checkout" name="checkin" />
           </div>
           <div>
             <label for="check-out-date">Дата выезда</label>
-            <input id="check-out-date" type="date" value="2021-05-13" min="2021-05-11" max="2021-06-30" name="checkout" />
+            <input id="check-out-date" type="date" value="${frmtDate(dateOut, '-')}" min="${minDateInStr}" max="${maxDateOutStr}" name="checkout" />
           </div>
           <div>
             <label for="max-price">Макс. цена суток</label>
